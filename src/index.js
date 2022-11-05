f15924608b3ab6f7bee344b28f9332ab5fe7646e
Marc Capde <capasc5@gmail.com>
2022-11-05 14:53:07 +0200

    solving conf

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(  
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+  );
 
