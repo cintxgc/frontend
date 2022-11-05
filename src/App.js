d455c5ef1873dc16f0ec29470a792de2d02d355f
Cinta <cigonci01@gmail.com>
2022-11-05 11:57:06 +0200

    grafica triple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import TradeViewChart from 'react-crypto-chart';
+import './style.css';
+import React from 'react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <h3>ETH/USDT</h3>
+      <TradeViewChart
+        containerStyle={{
+          minHeight: '300px',
+          minWidth: '400px',
+          marginBottom: '30px',
+        }}
+        pair="ETHUSDT"
+      />
     </div>
   );
 }
-
-export default App;
